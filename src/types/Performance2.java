9125db31ff76de5b5e5fe19593161b75e742160c
Tanel Paal Desktop <ptan02@gmail.com>
2024-02-11 15:52:52 +0200

    Performance2 solution?

--- src/types/Performance2.java
+++ src/types/Performance2.java
@@ -7,6 +7,7 @@ public class Performance2 {
         double start = System.currentTimeMillis();
 
         for (int i = 0; i < 1e9; i++) {
+            i += 0;
         }
 
         System.out.println((System.currentTimeMillis() - start) / 1000);
